7480ee0b0392144a5a9ebacefd31f19a799c07b0
go2ramen <borcektrash@mail.ru>
2022-05-09 01:49:07 +0600

    feat: added basic keyboard with keydown/keyup events

---
+++ src/modules/Key.js
@@ -0,0 +1,11 @@
+export { Key };
+
+class Key {
+    constructor(classKey, row, keyNumber, item) {
+      this.classKey = classKey;
+      this.row = row;
+      this.keyNumber = keyNumber;
+      this.item = item;
+      
+    }
+  }
